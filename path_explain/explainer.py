4ab0579e613fa4868119b391fc7eb5ac624fab92
psturmfels <psturm@umich.edu>
2019-11-30 17:53:32 -0800

    okay the interaction code compiles now which is good

--- path_explain/explainer.py
+++ path_explain/explainer.py
@@ -9,14 +9,36 @@ class Explainer():
     package in terms of API.
     """
 
-    def attributions(self, inputs, baseline,
-                     batch_size=50, num_samples=100,
-                     use_expectation=True, output_indices=None,
+    def attributions(self,
+                     inputs,
+                     baseline,
+                     batch_size=50,
+                     num_samples=100,
+                     use_expectation=True,
+                     output_indices=None,
                      verbose=False):
         """
         A function that returns the path attributions for the
         given inputs.
         """
         raise Exception("Attributions have not been implemented " + \
+                    "for this class. Likely, you have imported " + \
+                    "the wrong class from this package.")
+
+    def interactions(self, inputs,
+                     baseline,
+                     batch_size=50,
+                     num_samples=100,
+                     use_expectation=True,
+                     output_indices=None,
+                     verbose=False,
+                     explain_index=None,
+                     interation_index=None,
+                     interaction_index=None):
+        """
+        A function that returns the path interactions for the
+        given inputs.
+        """
+        raise Exception("Interactions have not been implemented " + \
                     "for this class. Likely, you have imported " + \
                     "the wrong class from this package.")
--- path_explain/path_explainer_tf.py
+++ path_explain/path_explainer_tf.py
@@ -119,11 +119,43 @@ class PathExplainerTF(Explainer):
                     batch_predictions = batch_predictions[:, output_index]
 
             batch_gradients = tape.gradient(batch_predictions, batch_interpolated)
+
             batch_attributions = batch_gradients * batch_difference
             attribution_array.append(batch_attributions)
         attribution_array = np.concatenate(attribution_array, axis=0)
         attributions = np.mean(attribution_array, axis=0)
         return attributions
+
+    def _init_array(self,
+                    inputs,
+                    output_indices,
+                    interaction_index=None):
+        """
+        Internal helper function to get an
+        array of the proper shape.
+
+        Args:
+            See self.attributions for definitions.
+        """
+        test_output = self.model(inputs[0:1])
+        is_multi_output = len(test_output.shape) > 1
+        shape_tuple = inputs.shape
+        num_classes = test_output.shape[-1]
+
+        if interaction_index is None:
+            shape_tuple = (inputs.shape[0], ) + \
+                          2 * (inputs.shape[1:])
+
+        if is_multi_output and output_indices is None:
+            num_classes = test_output.shape[-1]
+            attributions = np.zeros((num_classes,) + shape_tuple)
+        elif not is_multi_output and output_indices is not None:
+            raise ValueError('Provided output_indices but ' + \
+                             'model is not multi output!')
+        else:
+            attributions = np.zeros(shape_tuple)
+
+        return attributions, is_multi_output, num_classes
 
     def attributions(self, inputs, baseline,
                      batch_size=50, num_samples=100,
@@ -156,17 +188,8 @@ class PathExplainerTF(Explainer):
                              index the output output_indices[i] for
                              the input inputs[i].
         """
-        test_output = self.model(inputs[0:1])
-        is_multi_output = len(test_output.shape) > 1
-
-        if is_multi_output and output_indices is None:
-            num_classes = test_output.shape[-1]
-            attributions = np.zeros((num_classes,) + inputs.shape)
-        elif not is_multi_output and output_indices is not None:
-            raise ValueError('Provided output_indices but ' + \
-                             'model is not multi output!')
-        else:
-            attributions = np.zeros(*inputs.shape)
+        attributions, is_multi_output, num_classes = self._init_array(inputs,
+                                                                      output_indices)
 
         input_iterable = enumerate(inputs)
         if verbose:
@@ -211,3 +234,185 @@ class PathExplainerTF(Explainer):
                                                                 None)
                 attributions[i] = current_attributions
         return attributions
+
+    def _single_interaction(self, current_input, current_baseline,
+                            current_alphas, num_samples, batch_size,
+                            use_expectation, output_index,
+                            interaction_index):
+        """
+        A helper function to compute path
+        interactions for a single sample.
+
+        Args:
+            current_input: A single sample. Assumes that
+                           it is of shape (...) where ...
+                           represents the input dimensionality
+            baseline: A tensor representing the baseline input.
+            current_alphas: Which alphas to use when interpolating
+            num_samples: The number of samples to draw
+            batch_size: Batch size to input to the model
+            use_expectation: Whether or not to sample the baseline
+            output_index: Whether or not to index into a given class
+            interaction_index: The index to take the interactions with respect to.
+        """
+        current_input = np.expand_dims(current_input, axis=0)
+        current_alphas = tf.reshape(current_alphas, (num_samples,) + \
+                                    (1,) * (len(current_input.shape) - 1))
+
+        attribution_array = []
+        for j in range(0, num_samples, batch_size):
+            number_to_draw = min(batch_size, num_samples - j)
+
+            batch_baseline = self._sample_baseline(current_baseline,
+                                                   number_to_draw,
+                                                   use_expectation)
+            batch_alphas = current_alphas[j:min(j + batch_size, num_samples)]
+
+            reps = np.ones(len(current_input.shape)).astype(int)
+            reps[0] = number_to_draw
+            batch_input = tf.convert_to_tensor(np.tile(current_input, reps))
+
+            batch_difference = batch_input - batch_baseline
+            batch_interpolated = batch_alphas * batch_input + \
+                                 (1.0 - batch_alphas) * batch_baseline
+
+            with tf.GradientTape() as second_order_tape:
+                second_order_tape.watch(batch_interpolated)
+                with tf.GradientTape() as first_order_tape:
+                    first_order_tape.watch(batch_interpolated)
+
+                    batch_predictions = self.model(batch_interpolated)
+                    if output_index is not None:
+                        batch_predictions = batch_predictions[:, output_index]
+
+                #Same shape as the input, e.g. [batch_size, ...]
+                batch_gradients = first_order_tape.gradient(batch_predictions, batch_interpolated)
+                if interaction_index is not None:
+                    if isinstance(interaction_index, int):
+                        interaction_index = [interaction_index]
+                    else:
+                        interaction_index = list(interaction_index)
+                    batch_gradients = batch_gradients[tuple([slice(None)] + interaction_index)]
+
+            if interaction_index is not None:
+                #In this case, the hessian is the same size as the input because we
+                #indexed into a particular gradient
+                batch_hessian = second_order_tape.gradient(batch_gradients, batch_interpolated)
+            else:
+                #In this case, the hessian matrix duplicates the dimensionality of the
+                #input. That is, if we have 5 CIFAR10 images for example, the
+                #input is of shape [5, 32, 32, 3]. The hessian will be of
+                #shape [5, 32, 32, 3, 5, 32, 32, 3]. This can
+                #be very memory intesive - there are other strategies,
+                #but this one sacrifices memory to be fast
+                batch_hessian = second_order_tape.jacobian(batch_gradients, batch_interpolated)
+                batch_hessian = batch_hessian.numpy()
+
+                #We index into the non-zero elements to get
+                #the shape [5, 32, 32, 3, 32, 32, 3]
+                hessian_index_array = [np.arange(number_to_draw)] + \
+                                      [slice(None)] * (len(current_input.shape) - 1)
+                hessian_index_array = 2 * hessian_index_array
+                batch_hessian = batch_hessian[tuple(hessian_index_array)]
+
+                #Now we adjust the batch_difference tensor to
+                #be ready to broadcast
+                for _ in range(len(current_input.shape) - 1):
+                    batch_difference = tf.expand_dims(batch_difference, axis=-1)
+
+            batch_attributions = batch_hessian * batch_difference
+            attribution_array.append(batch_attributions)
+        attribution_array = np.concatenate(attribution_array, axis=0)
+        attributions = np.mean(attribution_array, axis=0)
+        return attributions
+
+    def interactions(self, inputs, baseline,
+                     batch_size=50, num_samples=100,
+                     use_expectation=True, output_indices=None,
+                     verbose=False, explain_index=None,
+                     interation_index=None, interaction_index=None):
+        """
+        A function to compute path interactions (attributions of
+        attributions) on the given inputs.
+
+        Args:
+            inputs: A tensor of inputs to the model of shape (batch_size, ...).
+            baseline: A tensor of inputs to the model of shape
+                      (num_refs, ...) where ... indicates the dimensionality
+                      of the input.
+            batch_size: The maximum number of inputs to input into the model
+                        simultaneously.
+            num_samples: The number of samples to use when computing the
+                         expectation or integral.
+            use_expectation: If True, this samples baselines and interpolation
+                             constants uniformly at random (expected gradients).
+                             If False, then this assumes num_refs=1 in which
+                             case it uses the same baseline for all inputs,
+                             or num_refs=batch_size, in which case it uses
+                             baseline[i] for inputs[i] and takes 100 linearly spaced
+                             points between baseline and input (integrated gradients).
+            output_indices:  If this is None, then this function returns the
+                             attributions for each output class. This is rarely
+                             what you want for classification tasks. Pass an
+                             integer tensor of shape [batch_size] to
+                             index the output output_indices[i] for
+                             the input inputs[i].
+            interaction_index: Either None or an index into the input. If the latter,
+                               will compute the interactions with respect to that
+                               feature. This parameter should index into a batch
+                               of inputs as inputs[(slice(None) + interaction_index)].
+                               For example, if you had images of shape (32, 32, 3)
+                               and you wanted interactions with respect
+                               to pixel (i, j, c), you should pass
+                               interaction_index=[i, j, c].
+        """
+        interactions, is_multi_output, num_classes = self._init_array(inputs,
+                                                                      output_indices,
+                                                                      interaction_index)
+
+        input_iterable = enumerate(inputs)
+        if verbose:
+            input_iterable = enumerate(tqdm(inputs))
+
+        for i, current_input in input_iterable:
+            current_alphas = self._sample_alphas(num_samples, use_expectation)
+
+            if not use_expectation and baseline.shape[0] > 1:
+                current_baseline = np.expand_dims(baseline[i], axis=0)
+            else:
+                current_baseline = baseline
+
+            if is_multi_output:
+                if output_indices is not None:
+                    output_index = output_indices[i]
+                    current_interactions = self._single_interaction(current_input,
+                                                                    current_baseline,
+                                                                    current_alphas,
+                                                                    num_samples,
+                                                                    batch_size,
+                                                                    use_expectation,
+                                                                    output_index,
+                                                                    interaction_index)
+                    interactions[i] = current_interactions
+                else:
+                    for output_index in range(num_classes):
+                        current_interactions = self._single_interaction(current_input,
+                                                                        current_baseline,
+                                                                        current_alphas,
+                                                                        num_samples,
+                                                                        batch_size,
+                                                                        use_expectation,
+                                                                        output_index,
+                                                                        interaction_index)
+                        interactions[output_index, i] = current_interactions
+            else:
+                current_interactions = self._single_interaction(current_input,
+                                                                current_baseline,
+                                                                current_alphas,
+                                                                num_samples,
+                                                                batch_size,
+                                                                use_expectation,
+                                                                None,
+                                                                interaction_index)
+                interactions[i] = current_interactions
+        return interactions
