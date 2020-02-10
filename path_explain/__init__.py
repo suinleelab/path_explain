47d5e03a2444909ad9d7cfd05dd9b4fc290c1757
psturmfels <psturm@cs.washington.edu>
2020-02-10 15:21:41 -0800

    renaming

--- path_explain/__init__.py
+++ path_explain/__init__.py
@@ -2,3 +2,10 @@
 A module for explaining the output of gradient-based
 models using path attributions.
 """
+__version__ = '1.0'
+
+from .explainers.path_explainer_tf import PathExplainerTF
+from utils import set_up_environment, softplus_activation
+from .plot.scatter import scatter_plot
+from .plot.summary import summary_plot
+from .plot.text import text_plot, matrix_interaction_plot, bar_interaction_plot
