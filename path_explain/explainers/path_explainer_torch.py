f6c3c080d6d66e44581bba356f9a772da4f74b82
Joseph Janizek <jjanizek@uw.edu>
2020-02-25 16:17:33 -0800

    Add PyTorch Explainer

---
+++ path_explain/explainers/path_explainer_torch.py
@@ -0,0 +1,272 @@
+#!/usr/bin/env python
+import functools
+import operator
+import torch
+from torch.autograd import grad
+import numpy as np
+from tqdm import *
+
+def gather_nd(params, indices):
+    """
+    Args:
+        params: Tensor to index
+        indices: k-dimension tensor of integers. 
+    Returns:
+        output: 1-dimensional tensor of elements of ``params``, where
+            output[i] = params[i][indices[i]]
+            
+            params   indices   output
+
+            1 2       1 1       4
+            3 4       2 0 ----> 5
+            5 6       0 0       1
+    """
+    max_value = functools.reduce(operator.mul, list(params.size())) - 1
+    indices = indices.t().long()
+    ndim = indices.size(0)
+    idx = torch.zeros_like(indices[0]).long()
+    m = 1
+
+    for i in range(ndim)[::-1]:
+        idx += indices[i]*m
+        m *= params.size(i)
+
+    idx[idx < 0] = 0
+    idx[idx > max_value] = 0
+    return torch.take(params, idx)
+
+class PathExplainerTorch(object):
+    def __init__(self, model):
+        self.model = model
+        return
+    
+    def _get_ref_tensor(self,baseline,batch_size,num_samples):
+        number_to_draw = num_samples * batch_size
+        replace = baseline.shape[0] < number_to_draw
+        sample_indices = np.random.choice(baseline.shape[0],
+                                          size=number_to_draw,
+                                          replace=replace)
+        ref_tensor = baseline[sample_indices,:]
+        
+        return ref_tensor
+
+    def _get_samples_input(self, input_tensor, baseline, 
+                           num_samples, use_expectation):
+        '''
+        calculate interpolation points
+        Args:
+            input_tensor: Tensor of shape (batch, ...), where ... indicates
+                          the input dimensions. 
+            reference_tensor: A tensor of shape (batch, k, ...) where ... 
+                indicates dimensions, and k represents the number of background 
+                reference samples to draw per input in the batch.
+        Returns: 
+            samples_input: A tensor of shape (batch, k, ...) with the 
+                interpolated points between input and ref.
+            samples_delta: A tensor of shape (batch, 1, ...) with the
+                difference between input and reference for each sample
+        '''
+        input_dims = list(input_tensor.size())[1:]
+        num_input_dims = len(input_dims)
+        batch_size = input_tensor.size()[0]
+        
+        if use_expectation:
+            reference_tensor = self._get_ref_tensor(baseline,batch_size,num_samples)
+            shape = reference_tensor.shape
+            reference_tensor = reference_tensor.view(
+                    batch_size, 
+                    num_samples, 
+                    *(shape[1:]))
+            
+            # Grab a [batch_size, k]-sized interpolation sample
+            t_tensor = torch.FloatTensor(batch_size, num_samples).uniform_(0,1).to(reference_tensor.device)
+            shape = [batch_size, num_samples] + [1] * num_input_dims
+            interp_coef = t_tensor.view(*shape)
+
+            # Evaluate the end points
+            end_point_ref = (1.0 - interp_coef) * reference_tensor
+
+            input_expand_mult = input_tensor.unsqueeze(1)
+            end_point_input = interp_coef * input_expand_mult
+
+            # Affine Combine
+            samples_input = end_point_input + end_point_ref
+            
+        else:
+            batch_size = input_tensor.size()[0]
+            input_expand = input_tensor.unsqueeze(1)
+            reps = np.ones(len(baseline.shape)).astype(int)
+            reps[0] = batch_size
+            reference_tensor = baseline.repeat(list(reps)).unsqueeze(1)
+#             reference_tensor = torch.as_tensor(sampled_baseline).unsqueeze(1).to(baseline.device)
+            scaled_inputs = [reference_tensor + (float(i)/num_samples)*(input_expand - reference_tensor) \
+                             for i in range(0,num_samples+1)]
+            samples_input = torch.cat(scaled_inputs,dim=1)
+        
+        samples_delta = self._get_samples_delta(input_tensor, reference_tensor)
+        samples_delta = samples_delta.to(samples_input.device)
+        
+        return samples_input, samples_delta
+    
+    def _get_samples_delta(self, input_tensor, reference_tensor):
+        input_expand_mult = input_tensor.unsqueeze(1)
+        sd = input_expand_mult - reference_tensor
+        return sd
+    
+    def _get_grads(self, samples_input, output_indices=None):
+
+        grad_tensor = torch.zeros(samples_input.shape).float().to(samples_input.device)
+        
+        k_ = samples_input.shape[1]
+
+        for i in range(k_):
+            particular_slice = samples_input[:,i]
+            batch_output = self.model(particular_slice)
+            # should check that users pass in sparse labels
+            # Only look at the user-specified label
+            if batch_output.size(1) > 1:
+                sample_indices = torch.arange(0,batch_output.size(0)).to(samples_input.device)
+                indices_tensor = torch.cat([
+                        sample_indices.unsqueeze(1), 
+                        output_indices.unsqueeze(1)], dim=1)
+                batch_output = gather_nd(batch_output, indices_tensor)
+
+            model_grads = grad(
+                    outputs=batch_output,
+                    inputs=particular_slice,
+                    grad_outputs=torch.ones_like(batch_output).to(samples_input.device),
+                    create_graph=True)
+            grad_tensor[:,i,:] = model_grads[0]
+        return grad_tensor
+           
+    def attributions(self, input_tensor, baseline,
+                     num_samples = 50, use_expectation=True, 
+                     output_indices=None):
+        """
+        Calculate either Expected or Integrated Gradients approximation of 
+        Aumann-Shapley values for the sample ``input_tensor``.
+
+        Args:
+            model (torch.nn.Module): Pytorch neural network model for which the
+                output should be explained.
+            input_tensor (torch.Tensor): Pytorch tensor representing the input
+                to be explained.
+            baseline (torch.Tensor): Pytorch tensor representing the baseline.
+                If use_expectation is true, then baseline should be shape
+                (num_refs, ...) where ... indicates the dimensionality
+                of the input. Otherwise, baseline should be shape (1, ...).
+            output_indices (optional, default=None): For multi-class prediction
+        """
+        equal_dims = baseline.shape[1:] == input_tensor.shape[1:]
+        almost_equal_dims = baseline.shape == input_tensor.shape[1:]
+        
+        dev = input_tensor.device
+        baseline = baseline.to(dev)
+        
+        input_tensor.requires_grad_ = True
+        
+        if use_expectation and not equal_dims:
+            raise ValueError('baseline should be shape (num_refs, ...) \
+                              where ... indicates the dimensionality   \
+                              of the input')
+            
+        if not use_expectation and baseline.shape[0] != 1:
+            if almost_equal_dims:
+                baseline = baseline.unsqueeze(0)
+            else:
+                raise ValueError('baseline should be shape (...)           \
+                                  where ... indicates the dimensionality   \
+                                  of the input')
+        
+        samples_input, samples_delta = self._get_samples_input(input_tensor, baseline,
+                                                               num_samples, use_expectation)
+        grad_tensor = self._get_grads(samples_input, output_indices)
+        mult_grads = samples_delta * grad_tensor
+        attributions = mult_grads.mean(1)
+        
+        return attributions
+    
+    def interactions(self, input_tensor, baseline,
+                     num_samples=50, use_expectation=True,
+                     output_indices=None, interaction_index=None,
+                     verbose=True):
+        """
+        samples_input: A tensor of shape (batch, k, features)
+        ig_tensor: also size (batch, k, features), but contains IG values
+        
+        """
+        
+        if len(input_tensor.shape) != 2:
+            raise ValueError('PyTorch Explainer only supports ' + \
+                             'interaction for 2D input tensors!')
+        
+        equal_dims = baseline.shape[1:] == input_tensor.shape[1:]
+        almost_equal_dims = baseline.shape == input_tensor.shape[1:]
+        
+        if use_expectation and not equal_dims:
+            raise ValueError('baseline should be shape (num_refs, ...) \
+                              where ... indicates the dimensionality   \
+                              of the input')
+            
+        if not use_expectation and baseline.shape[0] != 1:
+            if almost_equal_dims:
+                baseline = baseline.unsqueeze(0)
+            else:
+                raise ValueError('baseline should be shape (...)           \
+                                  where ... indicates the dimensionality   \
+                                  of the input')
+        
+        inner_loop_nsamples = int(round(np.sqrt(num_samples)))
+        
+        samples_input, samples_delta = self._get_samples_input(input_tensor, baseline,
+                                                               inner_loop_nsamples, use_expectation)
+        
+        if interaction_index is not None:
+            interaction_mult_tensor = torch.zeros([input_tensor.size(0), samples_input.size(1), input_tensor.size(1)])
+        else:
+            interaction_mult_tensor = torch.zeros([input_tensor.size(0), samples_input.size(1), 
+                                                   input_tensor.size(1), input_tensor.size(1)])
+            
+        ig_tensor = torch.zeros(samples_input.shape).float()
+        
+        if use_expectation:
+            loop_num = inner_loop_nsamples
+        else:
+            loop_num = inner_loop_nsamples + 1
+        
+        if verbose:
+            iterable = tqdm(range(loop_num))
+        else:
+            iterable = range(loop_num)
+        
+        for i in iterable:
+            
+            particular_slice = samples_input[:,i]
+            ig_tensor[:,i,:] = self.attributions(particular_slice, baseline,
+                                                 num_samples=inner_loop_nsamples, use_expectation=use_expectation)
+            
+            if interaction_index is not None:
+                second_grads = grad(
+                        outputs=ig_tensor[:,i,interaction_index],
+                        inputs=particular_slice,
+                        grad_outputs=torch.ones_like(ig_tensor[:,i,interaction_index]),
+                        create_graph=True)[0]
+                interaction_mult_tensor[:,i,:] = second_grads
+
+            else:
+                for feature in range(input_tensor.size(1)):
+                    second_grads = grad(
+                        outputs=ig_tensor[:,i,feature],
+                        inputs=particular_slice,
+                        grad_outputs=torch.ones_like(ig_tensor[:,i,feature]),
+                        create_graph=True)[0]
+                    interaction_mult_tensor[:,i,feature,:] = second_grads
+
+        interaction_mult_tensor = interaction_mult_tensor.to(samples_delta.device)
+        if interaction_index is not None:
+            interaction_tensor = interaction_mult_tensor * samples_delta
+        else:
+            interaction_tensor = interaction_mult_tensor * samples_delta.unsqueeze(2)
+        interactions = interaction_tensor.mean(1)
+        
+        return interactions
